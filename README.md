906bddfb542e76e4a8144e0e1b87f5dce9081f90
Azinum <lucaskarlsson@This.local>
2016-08-30 11:51:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# The Falk Programming Language
---
+++ main.c
@@ -0,0 +1,10 @@
+/* main.c */
+/* author: Azinum */
+/* date: 30/08/16 */
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
