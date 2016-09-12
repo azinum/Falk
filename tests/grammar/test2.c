7a1188925e43aca1aacb65136ef4d59bdec68f6e
Azinum <lucaskarlsson@This.lan>
2016-09-12 18:51:16 +0200

    Update grammar test file
    + Added syntax test file

--- tests/grammar/test2.c
+++ tests/grammar/test2.c
@@ -9,15 +9,15 @@
 #define arr_size(ARR) (sizeof(ARR) / sizeof(ARR[0]))
 
 enum Flags {
-    IF = -8,
-    WHILE,
-    FUNC,
-
     NONE        = 2 << 0,
-    EXPRESSION  = 2 << 1,
-    BODY        = 2 << 2,
-    END         = 2 << 3,
+    IF          = 2 << 1,
+    WHILE       = 2 << 2,
+    FUNC        = 2 << 3,
+    EXPRESSION  = 2 << 4,
+    BODY        = 2 << 5,
+    END         = 2 << 6,
 };
+
 
 typedef struct Rule {
     int value;      /* rule itself */
@@ -25,9 +25,16 @@ typedef struct Rule {
     int flagc;      /* flag count */
 } Rule;
 
+Rule rules[] = {
+    {NONE, NULL, -1},
+    {IF, NULL, -1},
+    {WHILE, NULL, -1},
+    {FUNC, NULL, -1},
+};
+
 int main() {
     int prog[] = {
-        IF, EXPRESSION, END
+        IF, EXPRESSION, BODY, END
     };
 
     Rule rule;
@@ -43,12 +50,19 @@ int main() {
                 int value;
                 for (int j = 0; j < rule.flagc; j++) {
                     value = prog[++i] & rule.flags[j];
-                    if (!value) {
+                    if (!value || i >= arr_size(prog)) {
                         printf("Grammatical error\n");
                     }
                 }
             }
                 break;
+
+            case WHILE: {
+                /* check grammar for while */
+            }
+                break;
+
+            /* case ... */
 
             default:
                 break;
