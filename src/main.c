ad2ca4887b125f98178a03c86ba9d5c26fff6778
Azinum <lucaskarlsson@This.local>
2016-11-11 06:48:07 +0100

    Cleaned up main

--- src/main.c
+++ src/main.c
@@ -2,33 +2,18 @@
 /* Author: Azinum */
 /* Date: 2016-08-30 */
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "falk.h"
-#include "ast.h"
 
-int main(int argc, const char* argv[]) {
+
+int main(int argc, char** argv) {
     Falk_instance* F = new(Falk_instance);
-    falk_instance_init(F);
-    
-    AST_node* node = new(AST_node);
-    ast_node_init(node, node);
-    ast_node_push_child_value(node, node, falk_create_token("A", T_STRING));
-    ast_node_push_child_value(node, node, falk_create_token("B", T_STRING));
-    ast_node_new_branch(node->children, node);
-    ast_node_push_child_value(ast_node_get_child(node, 0), node, falk_create_token("a", T_STRING));
-    ast_node_push_child_value(ast_node_get_child(node, 0), node, falk_create_token("b", T_STRING));
-    
-    int it = 100;
-    while (it-- > 0)        /* walk ast 100 times */
-        ast_walk_ast(node, 0);
-    
-    /* Falk_instance* F = new(Falk_instance);
+    if (!falk_instance_init(F)) {
+        printf("%s\n", "Falk instance could not be created.");
+        return 0;
+    }
     F->argc = argc;
     F->argv = argv;
-    falk_instance_init(F);
     falk_execute(F);
-    */
-    return 0;
+    falk_instance_free(F);
+    return 1;
 }
