e9c02f4c230d793a59e5c2d7ed91d70d478ce1b9
Azinum <lucaskarlsson@This.local>
2016-11-22 12:24:54 +0100

    Added AST node types

--- src/ast.h
+++ src/ast.h
@@ -15,6 +15,16 @@ typedef struct AST_node {
     Token value;
 } AST_node;
 
+enum AST_node_types {
+    AST_NODE_EXPR,      /* (...) */
+    AST_NODE_BODY,      /* {...} */
+};
+
+static const char* ast_node_identifiers[] = {
+    "expression",
+    "body"
+};
+
 int ast_node_init(AST_node* node, AST_node* root);
 
 int ast_node_push_child(AST_node* node, AST_node* child);
