62245c677bbd4a75b1abbfe2a60b8de6c0c85705
Azinum <lucaskarlsson@This.local>
2016-11-28 14:12:16 +0100

    Changed lib name
    * From libstd -> libtest.

---
+++ src/libtest.c
@@ -0,0 +1,44 @@
+/* libtest.c */
+/* Author: Azinum */
+/* Date: 2016-11-28 */
+
+#include <time.h>
+
+#include "include/falk.h"
+
+int fib(int n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+static Object falk_fib(VM_instance* VM) {
+    int ret, n;
+    if (!falk_build_args(VM, "i", &n)) {
+        return falk_create_null(VM);
+    }
+    ret = fib(n);
+    return falk_create_object(VM, 'i', &ret);
+}
+
+static Object falk_print(VM_instance* VM) {
+    if (VM->stack->top > 0) {
+        print_object(list_get_top(VM->stack));
+        vm_stack_pop();
+        return falk_create_null(VM);
+    }
+    VM_throw_error(VM, VM_ERR_STACK, VM_ERRC_STACK_NOT_ENOUGH_ITEMS, "falk_print lib function");
+    return falk_create_null(VM);
+}
+
+static struct CLibfunction mylib[] = {
+    {"fib", falk_fib, "Fib."},
+    {"print", falk_print, "Print to console."},
+    {NULL, NULL, NULL}
+};
+
+Object Init(VM_instance* VM) {
+    falk_openlib(VM, mylib);
+    return falk_create_null(VM);
+}
