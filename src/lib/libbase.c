ac8f3faccd3f7ce4794d27c70c149df78051592c
Azinum <lucaskarlsson@This.local>
2016-11-29 08:32:11 +0100

    Fixed print function in baselib

--- src/lib/libbase.c
+++ src/lib/libbase.c
@@ -5,14 +5,14 @@
 #include "include/falk.h"
 
 static Object print(VM_instance* VM) {
-    print_object(list_get_top(VM));
+    print_object(list_get_top(VM->stack));
     falk_pop(VM);
     return falk_create_null(VM);
 }
 
 static CLibfunction baselib[] = {
     {"print", {print, 1}, ""},
-    {NULL, {NULL, NULL}, NULL}
+    {NULL, {NULL, 0}, NULL}
 };
 
 Object Init(VM_instance* VM) {
